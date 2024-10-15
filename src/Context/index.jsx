e06303cf08f27455f6f63ff763f9847beaa4db01
Isabela <isa31297@gmail.com>
2024-10-15 14:52:08 -0500

    barra lateral detalles del producto

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from 'react'
 
 export const ShoppingCartContext = createContext()
 
-export const ShoppingCartProvider = ({children}) => {
+export const ShoppingCartProvider = ({ children }) => {
     const [count, setCount] = useState(0);
     return (
         <ShoppingCartContext.Provider value={{
