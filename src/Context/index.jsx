2e71f2b7339472d601a07962cd198a1ac13bdb1e
Isabela <isa31297@gmail.com>
2024-10-15 21:17:46 -0500

    se agrega la info a la barra lateral

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,10 +4,22 @@ export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({ children }) => {
     const [count, setCount] = useState(0);
+
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+    const openProductDetail = () => {setIsProductDetailOpen(true) 
+        console.log(isProductDetailOpen)}
+    const closeProductDetail = () => setIsProductDetailOpen(false)
+
+    const [productToShow, setProductToShow] = useState({});
     return (
         <ShoppingCartContext.Provider value={{
             count,
-            setCount
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow,
         }}>
             {children}
         </ShoppingCartContext.Provider>
