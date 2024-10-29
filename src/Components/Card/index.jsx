20011f538acae880ab90a061b3a63fbde242ae44
Isabela <isa31297@gmail.com>
2024-10-28 19:41:13 -0500

    se hizo una seccion lateral para el carrito

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -9,6 +9,15 @@ const Card = (data) => {
         context.openProductDetail()
         context.setProductToShow(ProductDetail)
     }
+
+    const addProductToCart = (event, productData)=> {
+        event.stopPropagation()
+        context.setCount(context.count + 1)
+        context.setCartProducts([...context.cartProducts, productData])
+        context.openCheckoutSideMenu()
+        context.closeProductDetail()
+        console.log('AAA ', context.cartProducts)
+    }
     return(
         <div 
         className='bg-white cursor-pointer w-56 h-60 rounded-lg'
@@ -17,7 +26,7 @@ const Card = (data) => {
                 <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-blue-950 text-xs m-2 px-1 py-0.5'>{data.data.category}</span>
                 <img className='w-full h-full object-cover rounded-lg' src={data.data.image}/>
                 <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
-                onClick={() => context.setCount(context.count + 1)}><PlusIcon className='size-3 text-black' /></div>
+                onClick={(event) => addProductToCart(event, data.data)}><PlusIcon  className='size-3 text-black' /></div>
             </figure>
             <p className='flex justify-between'>
                 <span className='text-sm font-light'>{data.data.title}</span>
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -5,12 +5,25 @@ export const ShoppingCartContext = createContext()
 export const ShoppingCartProvider = ({ children }) => {
     const [count, setCount] = useState(0);
 
+    //detalles del producto
     const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
-    const openProductDetail = () => {setIsProductDetailOpen(true) 
-        console.log(isProductDetailOpen)}
+    const openProductDetail = () => {setIsProductDetailOpen(true)}
+
     const closeProductDetail = () => setIsProductDetailOpen(false)
 
-    const [productToShow, setProductToShow] = useState({});
+    // Checkout Side Menu
+    const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false)
+    
+    const openCheckoutSideMenu = () => {setIsCheckoutSideMenuOpen(true) 
+        console.log('ssss :' , isCheckoutSideMenuOpen)}
+    
+    const closeCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(false) 
+
+    //Show products
+    const [productToShow, setProductToShow] = useState({})
+
+    //hace parte del shopping cart, se agregan los productos a la bolsa
+    const [cartProducts, setCartProducts] = useState([]);
     return (
         <ShoppingCartContext.Provider value={{
             count,
@@ -20,6 +33,11 @@ export const ShoppingCartProvider = ({ children }) => {
             isProductDetailOpen,
             productToShow,
             setProductToShow,
+            cartProducts,
+            setCartProducts,
+            isCheckoutSideMenuOpen,
+            openCheckoutSideMenu,
+            closeCheckoutSideMenu,
         }}>
             {children}
         </ShoppingCartContext.Provider>
