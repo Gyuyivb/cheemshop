9c416885a99f66a7974f9626ca30f2a54a9b284d
Isabela <isa31297@gmail.com>
2024-10-28 20:19:08 -0500

    se agregan productos al carrito

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -2,9 +2,12 @@ import { XMarkIcon } from '@heroicons/react/24/solid'
 import './styles.css'
 import { useContext } from 'react'
 import { ShoppingCartContext } from '../../Context'
+import OrderCard from '../OrderCard'
 
 const CheckoutSideMenu = () => {
     const context = useContext(ShoppingCartContext)
+    console.log('AAA ', context.cartProducts)
+
     return(
         <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
             <div className='flex justify-between items-center p-6'>
@@ -13,6 +16,18 @@ const CheckoutSideMenu = () => {
                     <XMarkIcon className="size-6 text-black cursor-pointer" 
                     onClick={() => context.closeCheckoutSideMenu()}/>
                 </div>
+            </div>
+            <div className='px-6'>
+                {
+                    context.cartProducts.map(product => (
+                        <OrderCard 
+                            key={product.id}
+                            title={product.title}
+                            imageUrl={product.image}
+                            price={product.price}
+                        />
+                    ))
+                }
             </div>
         </aside>
     )
