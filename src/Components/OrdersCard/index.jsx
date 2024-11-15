8f361a0893fb0a2456021b90f9e58ebf5ff37733
Isabela <isa31297@gmail.com>
2024-11-15 15:49:43 -0500

    se agrego estilos bonitos a las card de ordenes

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,14 +1,19 @@
-import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ChevronRightIcon } from '@heroicons/react/24/solid'
 
 const OrdersCard = props => {
     const { totalPrice, totalProducts } = props
     
     return (
-        <div className='flex justify-between items-center mb-2 border border-red-950'>
-            <p>
-                <span>01.03.34</span>
-                <span>{totalProducts}</span>
-                <span>{totalPrice}</span>
+        <div className='flex justify-between items-center mb-2 border border-red-950 rounded-lg p-4 w-80'>
+            <p className='flex justify-between w-full'>
+                <div className='flex flex-col'>
+                    <span className='font-light'>01.03.34</span>
+                    <span>{totalProducts} articles</span>
+                </div>
+                <div className='flex items-center gap-2'>
+                    <span className='font-medium text-2xl'>${totalPrice}</span>
+                    <ChevronRightIcon className='h-6 w-6 text-black cursor-pointer'></ChevronRightIcon>
+                </div>
             </p>
         </div>
     )
