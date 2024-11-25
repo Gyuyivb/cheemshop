e4c35df95e07e08ff60daae890b6c0fee387724f
Isabela <isa31297@gmail.com>
2024-11-25 11:23:50 -0500

    se logro el reto no como se queria

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -13,6 +13,10 @@ import './App.css'
 const AppRoutes = () => {
   let routes = useRoutes([
     { path: '/',element: <Home />},
+    { path: '/all',element: <Home />},
+    { path: '/clothes',element: <Home />},
+    { path: '/jewerly',element: <Home />},
+    { path: '/others',element: <Home />},
     { path: '/my-account', element: <MyAccount />},
     { path: '/my-order', element: <MyOrder />},
     { path: '/my-orders', element: <MyOrders />},
